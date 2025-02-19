4a7f761828020aa531662803fcb070b949e35cb8
Mrpeanizfaes <tnamhi0602@gmail.com>
2025-02-19 13:36:11 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const colors = ["green", "blue", "brown", "yellow", "red", "black"];
 
 export default function App() {
+  const [background, setBackground] = useState("green");
+
+  function handleOnPress(color) {
+    setBackground(color);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: background }]}>
+      {colors.map((color) => {
+        return (
+          <TouchableOpacity
+            key={color}
+            style={[styles.button, { backgroundColor: color }]}
+            onPress={() => handleOnPress(color)}
+          >
+            <Text style={styles.buttonText}>{color}</Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 }
@@ -13,8 +31,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    width: "80%",
+    padding: 20,
+    margin: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
